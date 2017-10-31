3eb9e977591352a2d812165744688309334fcb21
Manjunath R <mramasw2@cisco.com>
2017-10-31 13:02:09 +0530

    Added new class2

---
+++ SampleMaven/src/test/java/com/wip/test/Sample.java
@@ -0,0 +1,11 @@
+package com.wip.test;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Check is completed");
+
+	}
+
+}
